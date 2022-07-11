8bd17cae018ed99b03d0ed1ff60d6704d3af9105
Taras Nester <tarasnester414@gmail.com>
2022-07-11 10:33:56 +0300

    created models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    completed = models.BooleanField(default=False)
+    tags = models.ManyToManyField("Tag", related_name="tasks")
+
+    class Meta:
+        ordering = ["-completed", "-created"]
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=63)
